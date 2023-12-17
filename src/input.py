ad89b65cb3befd1b556d871f25e31bbae75890d2
TestDragon12 <95674450+TestDragon12@users.noreply.github.com>
2023-12-17 04:35:30 +0000

    corrected clean_results

--- src/input.py
+++ src/input.py
@@ -11,13 +11,22 @@ def main():
 
    
     
-    val_sur = validate_ans_3(no_dupl)
+    validated_survey = validate_ans_3(no_dupl)
 
     #ticket 6: Output the cleansed result data to a new file
 
     with open(r"clean_results.csv", "w") as cr:
-        for item in val_sur:
-            cr.write("%s\n" % item)
+        for item in validated_survey:
+            for i in item:
+                if item.index(i) < (len(item) - 1):
+                    cr.write("%s," % i)
+                    
+                else:
+                    cr.write("%s" % i)  
+            if validated_survey.index(item) < (len(validated_survey)-1):
+                cr.write("\n")
+        
+
 
     cr.close()
 
